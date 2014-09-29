efa072b9ddf1bb45dcada728a7cfa4222ff9fb8e
Leif Johansson <leifj@sunet.se>
2014-09-29 20:17:32 +0200

    a couple of tools

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -1,6 +1,6 @@
 # This manifest is managed using cosmos
 
 class cosmos::tools {
-   $tools = ["vim","traceroute","tcpdump","molly-guard","less","rsync","screen","strace","lsof"]
+   $tools = ["vim","traceroute","tcpdump","molly-guard","less","rsync","screen","strace","lsof","update-manager-core","unattended-upgrades"]
    package { $tools: ensure => latest}
 }
