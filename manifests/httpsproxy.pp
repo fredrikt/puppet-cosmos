5eecfe2d8b527680a39b090527971153e75c5fbc
Leif Johansson <leifj@sunet.se>
2014-10-15 02:21:24 +0200

    turn off sslv3

--- manifests/httpsproxy.pp
+++ manifests/httpsproxy.pp
@@ -14,6 +14,8 @@ class cosmos::httpsproxy ($certs = ['/etc/ssl/private/server.pem']) {
       require => Package['ssl-cert'],
    }
    Exec['generate-snakeoil'] -> File['/etc/ssl/private/server.pem']
+   $ssl_ciphers_dfri = "ECDHE-ECDSA-AES256-GCM-SHA384 ECDHE-RSA-AES256-GCM-SHA384 ECDHE-ECDSA-AES256-SHA384 ECDHE-RSA-AES256-SHA384 ECDHE-ECDSA-AES128-GCM-SHA256 ECDHE-RSA-AES128-GCM-SHA256 ECDHE-ECDSA-AES128-SHA256 ECDHE-RSA-AES128-SHA256 ECDHE-RSA-AES256-SHA DHE-RSA-AES256-SHA DHE-RSA-AES128-SHA AES256-SHA256"
+   $ssl_ciphers_eduid_pfs = "DHE-RSA-CAMELLIA256-SHA DHE-RSA-AES256-SHA DES-CBC3-SHA"
    pound::entry {"$name-httpsproxy-443":
       listen_ip => $ipaddress,
       listen_port => '443',
@@ -21,6 +23,7 @@ class cosmos::httpsproxy ($certs = ['/etc/ssl/private/server.pem']) {
       head_require => 'Host:.*',
       backend_ip => '127.0.0.1',
       backend_port => '80',
+      ssl_ciphers  => "!SSLv3 ${ssl_ciphers_dfri} ${ssl_ciphers_eduid_pfs}",
       server_cert => $certs
    }
    include augeas
